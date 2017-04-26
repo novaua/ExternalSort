e49e79b4cfa7b0bcfb577f35a9675b3a31e5e394
novaua <novaua@ukr.net>
2017-04-26 07:26:48 +0300

    Test on speed of individual file sort.

---
+++ ExternalSort.Tests/SortSpeedTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+using Common;
+
+namespace ExternalSort.Tests
+{
+    [TestClass]
+    public class SortSpeedTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var inputDir = @"..\..\..\bin\Debug";
+            var files = Directory.EnumerateFiles(inputDir);
+            foreach (var file in files.Where(x => Path.GetFileName(x).StartsWith("input") && x.EndsWith(".txt")))
+            {
+                SortFile(file);
+            }
+        }
+
+        private void SortFile(string inputFile)
+        {
+            var fi = new FileInfo(inputFile);
+            if (!fi.Exists)
+            {
+                throw new ArgumentException($"File '{inputFile}' does not exist!");
+            }
+
+            using (var asw = new AutoStopwatch($"Sorting file '{inputFile}' {BytesFormatter.Format(fi.Length)} ", fi.Length))
+            {
+                var lines = File.ReadAllLines(inputFile);
+                Array.Sort(lines);
+            }
+        }
+    }
+}
